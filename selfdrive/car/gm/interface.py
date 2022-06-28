0798cb58f7ce525ce92464818432c02be5617e27
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-06-27 19:08:37 -0700

    Increase P upper on lateral
    Turns are slightly off center and towards the outer part of the curve.
    
     Centering is a touch slow exciting turns.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -144,7 +144,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRateCost = 0.5
       ret.steerActuatorDelay = 0.
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.273], [0.01, 0.02]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.284], [0.01, 0.02]]
       ret.lateralTuning.pid.kdBP = [0.]
       ret.lateralTuning.pid.kdV = [0.3]  #corolla from shane fork : 0.725
       ret.lateralTuning.pid.kf = 0.00005
