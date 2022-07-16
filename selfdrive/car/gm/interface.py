270230050e90077ff6e65e57e86b7cb71497f835
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-07-16 11:34:53 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -42,7 +42,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     # return params.ACCEL_MIN, params.ACCEL_MAX
     accel_max_bp = [10., 20., 50.]
-    accel_max_v = [1.45, 1.68, 1.78]
+    accel_max_v = [1.55, 1.75, 1.85]
 
     return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
@@ -172,7 +172,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.283], [0.01, 0.021]]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [0.32]  
+      ret.lateralTuning.pid.kdV = [0.3]  
       ret.lateralTuning.pid.kf = 0.000045
       
       
@@ -182,7 +182,7 @@ class CarInterface(CarInterfaceBase):
       max_lat_accel = 2.7
       ret.lateralTuning.torque.kp = 2.0 / max_lat_accel
       ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
-      ret.lateralTuning.torque.ki = 0.2 / max_lat_accel
+      ret.lateralTuning.torque.ki = 0.19 / max_lat_accel
       ret.lateralTuning.torque.friction = 0.008
 
       ret.lateralTuning.torque.kd = 1.0
