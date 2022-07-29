01169f9f87cd34ca14c026148f78fc79ca19314f
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-07-28 18:25:12 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -135,9 +135,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.outerLoopGainBP = [10., 30.]
       ret.lateralTuning.indi.outerLoopGainV = [4.5, 7.0]
       ret.lateralTuning.indi.timeConstantBP = [10., 30.]
-      ret.lateralTuning.indi.timeConstantV = [1.7, 3.4]
-      ret.lateralTuning.indi.actuatorEffectivenessBP = [10., 30.]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [2.08, 2.]
+      ret.lateralTuning.indi.timeConstantV = [1.7, 3.5]
+      ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [2.]
       
       
     elif lateral_control == 'LQR':
@@ -182,7 +182,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.torque.useSteeringAngle = True
       ret.lateralTuning.torque.kp = 1.8 / max_lateral_accel
       ret.lateralTuning.torque.ki = 0.6 / max_lateral_accel
-      ret.lateralTuning.torque.kd = 4.0 / max_lateral_accel
+      ret.lateralTuning.torque.kd = 3.7 / max_lateral_accel
       ret.lateralTuning.torque.kf = 1.0 # use with custom torque ff
       ret.lateralTuning.torque.friction = 0.005
 
